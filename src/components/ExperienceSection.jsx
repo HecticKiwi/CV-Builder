92620b9b64f1547edc019e826009b0fd67e05c54
Danny <dannyduong4.00@outlook.com>
2022-08-24 17:02:50 -0600

    Convert ExperienceSection to function component

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,57 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class ExperienceSection extends Component {
-  render() {
+export default function ExperienceSection({
+  experience, handleChange, addItem, deleteItem,
+}) {
+  const experienceItems = experience.map((item, i) => {
     const {
-      experience, handleChange, addItem, deleteItem,
-    } = this.props;
-
-    const experienceItems = experience.map((item, i) => {
-      const {
-        company, city, position, description, from, to, key,
-      } = item;
-
-      return (
-        <div className="experience-item" key={key}>
-          <label htmlFor="company">
-            <span className="field-label">Company:</span>
-            <input type="text" name="company" id="company" className="field-input" value={company} onChange={(e) => handleChange(e, i)} />
-          </label>
-          <label htmlFor="city">
-            <span className="field-label">City:</span>
-            <input type="text" name="city" id="city" className="field-input" value={city} onChange={(e) => handleChange(e, i)} />
-          </label>
-          <label htmlFor="position">
-            <span className="field-label">Position:</span>
-            <input type="text" name="position" id="position" className="field-input" value={position} onChange={(e) => handleChange(e, i)} />
-          </label>
-          <label htmlFor="description">
-            <span className="field-label">Description:</span>
-            <input type="text" name="description" id="description" className="field-input" value={description} onChange={(e) => handleChange(e, i)} />
-          </label>
-          <div className="period">
-            <label htmlFor="from">
-              <span className="field-label">From:</span>
-              <input className="field-input" type="date" name="from" id="from" value={from} onChange={(e) => handleChange(e, i)} />
-            </label>
-            <label htmlFor="to">
-              <span className="field-label field-label--to">To:</span>
-              <input className="field-input" type="date" name="to" id="to" value={to} onChange={(e) => handleChange(e, i)} />
-            </label>
-          </div>
-          <div className="button-group">
-            <button className="delete" type="button" onClick={deleteItem}>Delete</button>
-            {i === experience.length - 1 && <button className="add" type="button" onClick={addItem}>Add</button>}
-          </div>
-        </div>
-      );
-    });
+      company, city, position, description, from, to, key,
+    } = item;
 
     return (
-      <section className="ExperienceSection">
-        <h2>Experience</h2>
-        {experienceItems}
-      </section>
+      <div className="experience-item" key={key}>
+        <label htmlFor="company">
+          <span className="field-label">Company:</span>
+          <input type="text" name="company" id="company" className="field-input" value={company} onChange={(e) => handleChange(e, i)} />
+        </label>
+        <label htmlFor="city">
+          <span className="field-label">City:</span>
+          <input type="text" name="city" id="city" className="field-input" value={city} onChange={(e) => handleChange(e, i)} />
+        </label>
+        <label htmlFor="position">
+          <span className="field-label">Position:</span>
+          <input type="text" name="position" id="position" className="field-input" value={position} onChange={(e) => handleChange(e, i)} />
+        </label>
+        <label htmlFor="description">
+          <span className="field-label">Description:</span>
+          <input type="text" name="description" id="description" className="field-input" value={description} onChange={(e) => handleChange(e, i)} />
+        </label>
+        <div className="period">
+          <label htmlFor="from">
+            <span className="field-label">From:</span>
+            <input className="field-input" type="date" name="from" id="from" value={from} onChange={(e) => handleChange(e, i)} />
+          </label>
+          <label htmlFor="to">
+            <span className="field-label field-label--to">To:</span>
+            <input className="field-input" type="date" name="to" id="to" value={to} onChange={(e) => handleChange(e, i)} />
+          </label>
+        </div>
+        <div className="button-group">
+          <button className="delete" type="button" onClick={deleteItem}>Delete</button>
+          {i === experience.length - 1 && <button className="add" type="button" onClick={addItem}>Add</button>}
+        </div>
+      </div>
     );
-  }
+  });
+
+  return (
+    <section className="ExperienceSection">
+      <h2>Experience</h2>
+      {experienceItems}
+    </section>
+  );
 }
